54e10891e3cf37b74beb5fc154bb813f391441e6
hemant <shroff.hemant@gmail.com>
2017-05-26 11:46:35 +0530

    updated code

--- app/src/main/java/com/network/networklib/MainActivity.java
+++ app/src/main/java/com/network/networklib/MainActivity.java
@@ -3,11 +3,15 @@ package com.network.networklib;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.network.networklibrary.CentralTendency;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //CentralTendency.arithmeticMean(10);
     }
 }
