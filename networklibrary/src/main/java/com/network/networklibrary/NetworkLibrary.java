1ef57e53517f7c14f789318f04301eee115b33ad
hemant <shroff.hemant@gmail.com>
2017-05-29 15:51:08 +0530

    networl library updated

--- networklibrary/src/main/java/com/network/networklibrary/NetworkLibrary.java
+++ networklibrary/src/main/java/com/network/networklibrary/NetworkLibrary.java
@@ -1,5 +1,13 @@
 package com.network.networklibrary;
 
+import android.content.Context;
+import android.support.v7.preference.PreferenceManager;
+
+import com.network.networklibrary.parsing.PreferenceKeys;
+
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Created by Hemant on 5/26/2017.
  */
@@ -8,11 +16,29 @@ public class NetworkLibrary {
 
     private static String base_url;
 
+    private PreferenceManager privateManager;
+    private static NetworkLibrary instance;
+    private Context context;
+
+    public static void init(Context context) {
+        if (instance == null)
+            instance = new NetworkLibrary(context);
+    }
+
+    private NetworkLibrary(Context context) {
+        this.context = context.getApplicationContext();
+
+        privateManager = new PreferenceManager(context);
+        privateManager.setSharedPreferencesName(PreferenceKeys.NAME);
+        privateManager.setSharedPreferencesMode(Context.MODE_PRIVATE);
+        privateManager.setStorageDeviceProtected();
+    }
+
     public static String getBase_url() {
-        return base_url;
+        return instance.privateManager.getSharedPreferences().getString(PreferenceKeys.BASE_URL, "");
     }
 
     public static void setBase_url(String base_url) {
-        NetworkLibrary.base_url = base_url;
+        instance.privateManager.getSharedPreferences().edit().putString(PreferenceKeys.BASE_URL, base_url).apply();
     }
 }
--- networklibrary/src/main/java/com/network/networklibrary/parsing/PreferenceKeys.java
+++ networklibrary/src/main/java/com/network/networklibrary/parsing/PreferenceKeys.java
@@ -12,7 +12,7 @@ public final class PreferenceKeys {
     public static final String NOTIFICATION_TRAVEL_REQUESTS = "notification_travel_requests";
     public static final String LOG_OUT = "log_out";
 
-    public static final String NAME = "com.xtempo.q2t.permanent_preferences";
+    public static final String NAME = "com.q2t.permanent_preferences";
 
     public static final String COMPANY_ID = "company_id";
     public static final String BASE_URL = "base_url";
