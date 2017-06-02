643bdaf1004744fd3ab3b0b86c84edeca4c66f82
hemant <shroff.hemant@gmail.com>
2017-06-02 11:27:59 +0530

    add bintray configuration

--- networklibrary/src/main/java/com/network/networklibrary/parsing/PreferenceKeys.java
+++ networklibrary/src/main/java/com/network/networklibrary/parsing/PreferenceKeys.java
@@ -6,14 +6,7 @@ package com.network.networklibrary.parsing;
 public final class PreferenceKeys {
     public static final String AUTH_KEY = "auth_key";
 
-    public static final String GCM_TOKEN = "gcm_token";
-
-    public static final String NOTIFICATION_ANNOUNCEMENTS = "notification_announcements";
-    public static final String NOTIFICATION_TRAVEL_REQUESTS = "notification_travel_requests";
-    public static final String LOG_OUT = "log_out";
-
     public static final String NAME = "com.q2t.permanent_preferences";
 
-    public static final String COMPANY_ID = "company_id";
     public static final String BASE_URL = "base_url";
 }
